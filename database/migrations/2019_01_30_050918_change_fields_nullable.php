39e68c26772cf9b0ba949defd98001513387af58
Menard Ysrael Cruz <ysrael214@gmail.com>
2019-02-02 18:47:21 +0800

    added appservice provider

--- database/migrations/2019_01_30_050918_change_fields_nullable.php
+++ database/migrations/2019_01_30_050918_change_fields_nullable.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeFieldsNullable extends Migration
+class ChangeFieldsNullable2 extends Migration
 {
     /**
      * Run the migrations.
